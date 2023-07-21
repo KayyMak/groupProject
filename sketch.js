d19f34801e44095ff774570bc765952f0a2a7cba
KayMakk <bomo794@gmail.com>
2023-07-21 14:56:45 -0700

    fixed bug

--- sketch.js
+++ sketch.js
@@ -41,39 +41,36 @@ function setup(){
     line(0, 400, 500, 400);
     fill('orange');
     rect(xPos, yPos, 50);
-    xPos += 2;
-    if (xPos > 450) {
-        xPos -=3 ;
-    }
+    
+
+    
     if (keyIsDown(LEFT_ARROW) || (keyIsDown(65))) { // wAsd
         if(xPos < 0){
             xPos = 0;
-        } else{
-            xPos -= 6;
-        }
-        
+        } 
+        xPos -= 3;
     }
     if (keyIsDown(RIGHT_ARROW) || (keyIsDown(68))) { // wasD
         if (xPos > 450) {
             xPos = 450;
-            xPos += 3;
+            
         }
-        
+        xPos += 3;
     }
     if (keyIsDown(UP_ARROW) || (keyIsDown(87) || keyIsDown(32))) { // Wasd
         if (yPos < 0) {
             yPos = 0;
-            yPos += 3;
+            
         }
+        yPos += 3;
     }
     if (keyIsDown(DOWN_ARROW) || (keyIsDown(83))) { // waSd 
         // if trying to past barrier, set yPos to 250
         // else, allowed to go down
         if(yPos > 350){
             yPos = 350;
-        }else{
-            yPos += 3;
         }
+        yPos += 3;
     }
 
     for (let i = 0; i < ballArray.length; i++) {
